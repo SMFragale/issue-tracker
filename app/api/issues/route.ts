71e00b5d2fb421528126c27265f860edcf6b9a13
Sebastian <smolanof@outlook.com>
2023-10-10 19:24:00 -0400

    Setup radix UI

---
+++ app/api/issues/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  //Insert new issue in database
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  console.log("Creating new issue");
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
+
+export async function GET(request: NextRequest) {
+  const issues = await prisma.issue.findMany();
+  return NextResponse.json(issues, { status: 200 });
+}
